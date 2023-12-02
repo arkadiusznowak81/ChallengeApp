076a6b37346ecd6870c12e50e4ddcba77264462e
Arek <arkadiusznowak81@gmail.com>
2023-12-02 22:51:55 +0100

    zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string imie = "Ewa";
+char plec = 'M';
+int wiek = 19;
+
+if (plec == 'K' && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (plec == 'M' && wiek < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("Warunki zadania nie zostały spełnione");
+}
